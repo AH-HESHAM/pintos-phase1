6c403e1d2a88ccbd0204aff1f945d2e9f5569aa3
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2023-04-26 17:01:26 +0200

    Update fixed_point.h

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -1,60 +1,47 @@
 #ifndef FIXED_POINT_H
 #define FIXED_POINT_H
+#include <stdint.h>
 
 #define FRACTIONAL_BITS 14
+typedef int fixed_t;
 
 // Convert an integer to a fixed-point representation
-int convert_to_fp(int x) {
-    return x * (1 << FRACTIONAL_BITS);
-}
+#define convert_to_fp(x) (x * (1 << FRACTIONAL_BITS))
+
+// Take integer part of a floating number
+#define takeInt(x) (x >> FRACTIONAL_BITS)
 
 // Convert a fixed-point representation to the nearest integer
-int convert_to_int(int x) {
-    if (x >= 0) {
-        return (x + (1 << (FRACTIONAL_BITS - 1))) >> FRACTIONAL_BITS;
-    } else {
-        return (x - (1 << (FRACTIONAL_BITS - 1))) >> FRACTIONAL_BITS;
-    }
-}
+
+#define convert_to_int(x) (x >= 0 ? ((x + (1 << (FRACTIONAL_BITS - 1))) >> FRACTIONAL_BITS) : ((x - (1 << (FRACTIONAL_BITS - 1))) >> FRACTIONAL_BITS))
+				
 
 // Add two fixed-point numbers
-int add_two_fp(int x, int y) {
-    return x + y;
-}
+#define add_two_fp( x,  y) (x + y)
 
 // Add an integer to a fixed-point number
-int add_to_fp(int x, int y) {
-    return x + (y << FRACTIONAL_BITS);
-}
+#define add_to_fp( x,  y) (x + (y << FRACTIONAL_BITS))
+
 
 // Subtract one fixed-point number from another
-int sub_two_fp(int x, int y) {
-    return x - y;
-}
+#define sub_two_fp( x,  y) (x-y)
 
 // Subtract an integer from a fixed-point number
-int sub_from_fp(int x, int y) {
-    return x - (y << FRACTIONAL_BITS);
-}
+#define sub_from_fp( x,  y) (x - (y << FRACTIONAL_BITS))
+
 
 // Multiply two fixed-point numbers
-int mul_two_fp(int x, int y) {
-    return ((int64_t) x) * y >> FRACTIONAL_BITS;
-}
+#define mul_two_fp( x,  y) ((fixed_t)(((int64_t) x) * y >> FRACTIONAL_BITS))
 
 // Multiply a fixed-point number by an integer
-int mul_to_fp(int x, int n) {
-    return x * n;
-}
+#define mul_to_fp( x,  n) (x * n)
+
 
 // Divide one fixed-point number by another
-int div_two_fp(int x, int y) {
-    return ((int64_t) x) << FRACTIONAL_BITS / y;
-}
+#define div_two_fp( x,  y) ((fixed_t)((((int64_t) x) << FRACTIONAL_BITS) / y))
+
 
 // Divide a fixed-point number by an integer
-int div_to_fp(int x, int n) {
-    return x / n;
-}
+#define div_to_fp( x,  n) (x/n)
 
 #endif
